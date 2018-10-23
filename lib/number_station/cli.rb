322f34e509ee6d145d77eabc58804e0a94faf003
David Kirwan <dkirwan@redhat.com>
2018-10-23 11:47:36 +0100

    Renamed convert_message to convert_to_phonetic

--- lib/number_station/cli.rb
+++ lib/number_station/cli.rb
@@ -25,6 +25,7 @@ require 'fileutils'
 module NumberStation
   class CLI < Thor
     
+
     # create_config
     desc "create_config [--path PATH]", "copy the sample config to current directory."
     long_desc <<-CREATE_CONFIG_LONG_DESC
@@ -59,8 +60,8 @@ module NumberStation
     end
 
 
-    # convert_message
-    desc "convert_message [MESSAGE]", "Convert a message to phonetic output."
+    # convert_to_phonetic
+    desc "convert_to_phonetic [MESSAGE]", "Convert a message to phonetic output."
     long_desc <<-CONVERT_MESSAGE_LONG_DESC
       convert_message takes a parameter which should point to a text file containing a message.
       Optional parameters:\n
@@ -73,7 +74,7 @@ module NumberStation
     option :intro, :type => :string
     option :outro, :type => :string
     option :mp3, :type => :string
-    def convert_message(message)
+    def convert_to_phonetic(message)
       NumberStation::ConfigReader.read_config()
 
       intro_path = options[:intro]
@@ -83,31 +84,29 @@ module NumberStation
       NumberStation.log.debug "intro_path: #{intro_path}" if options[:intro]
       NumberStation.log.debug "message_path: #{message_path}"
       NumberStation.log.debug "outro_path: #{outro_path}" if options[:outro]
-      NumberStation.log.debug "mp3_output: " if options[:mp3]
+      NumberStation.log.debug "mp3_output: #{mp3_output}" if options[:mp3]
 
       output = ""
       output += NumberStation.read_message(intro_path) if options[:intro]
       output += NumberStation.read_message(message_path)
       output += NumberStation.read_message(outro_path) if options[:outro]
-
       NumberStation.log.info "output: #{output}"
 
       if options[:mp3]
         NumberStation.log.debug "Generating mp3 output: #{mp3_path}"
         NumberStation.run(output, mp3_path)
       end
-
       return output
     end
 
 
-    # convert_message
-    desc "make_one_time_pad [--path PATH --num NUM --length LENGTH]", "Generate a one time pad of LENGTH containing NUM entries"
+    # make_one_time_pad
+    desc "make_one_time_pad [--path PATH --num-pads NUM --length LENGTH]", "Generate a one time pad of LENGTH containing NUM entries"
     long_desc <<-MAKE_ONE_TIME_PAD_LONG_DESC
     Generate a one time pad of LENGTH containing NUM entries
     Optional parameters:\n
       --path PATH\n
-      --num NUM\n
+      --num-pads NUM\n
       --length LENGTH
 
     If no parameters are passed it will generate 5 one time pads in the current 
@@ -119,17 +118,17 @@ module NumberStation
     def make_one_time_pad()
       NumberStation::ConfigReader.read_config()
       NumberStation.log.debug "make_one_time_pad"
-
+      
       length = options[:length]
       num_pads = options[:num_pads]
       path = options[:path]
-
       NumberStation.log.debug "length: #{length}" if options[:length]
       NumberStation.log.debug "num_pads: #{num_pads}" if options[:num_pads]
       NumberStation.log.debug "path: #{path}" if options[:path]
 
       NumberStation.make_otp(path, length, num_pads)
     end
+
 
   end
 end
